1f1fe4f60de54fa32f9740fca79305b06ee59771
marco <ianni-k@163.com>
2022-10-22 16:24:13 +0800

    T[number] meaning

--- easy-tuple/template.ts
+++ easy-tuple/template.ts
@@ -5,5 +5,6 @@ type TupleToObject<T extends readonly (string | number | symbol)[]> = {
 
 // typeof  value =>  type
 // as const   readonly
-
+// T[number]  map []
+ 
 const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
